2fe6865c4bdddc670131a9308734b360d9a29630
jono <26609800+jonomango@users.noreply.github.com>
2020-12-21 20:57:17 -0800

    template

--- template.py
+++ template.py
@@ -4,7 +4,6 @@ from functools import reduce
 
 with open("input2.txt", "r") as file:
   # iterate over every line in a file
-  for line in file.read().split("\n"):
+  for line in file.read().strip().split("\n"):
     print(line)
 
-print("solution =", 69)
